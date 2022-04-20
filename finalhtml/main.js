1f2e39a1c4b436aea640de1cb1af4583906f198b
Shino <104060506+1905dai@users.noreply.github.com>
2022-04-20 16:06:53 +0700

    Add files via upload

---
+++ finalhtml/main.js
@@ -0,0 +1,10 @@
+
+function notify(){
+  alert("Chức năng chưa được tạo");
+}
+function notify_in_web(){
+  alert("Đang ở chính trang này rồi !!");
+}
+function buy(){
+  alert("Đặt hàng thành công");
+}
